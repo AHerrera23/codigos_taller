71dd49f1dd9907f381d16c76ac5ca87a97d8ee8f
Abel Herrera <81653233+herrera22@users.noreply.github.com>
2023-05-23 11:01:47 -0300

    Add files via upload
    version numero 3 del codigo para mi landing page

---
+++ tp1-landing page/js/index.js
@@ -0,0 +1,7 @@
+// Chequeo del jQuery
+// Esta línea garantiza que todo lo que se ejecute dentro de ready
+// se ejecute solo cuando el HTML esté 100% cargado
+$(document).ready(() => {
+    console.log("en vivo neneeee");
+
+});
